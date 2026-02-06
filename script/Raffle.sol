a9cc94fc9c3940353258073641cdf4f36c7cd697
uttam <i.m.jaspritbumra@gmail.com>
2026-02-06 20:16:41 +0600

    Implement the Chainlink VRF

--- script/Raffle.sol
+++ script/Raffle.sol
@@ -21,14 +21,11 @@
 // private
 // view & pure functions
 
-
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
 import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
-
-
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
 
 /**
  * @title A smample Raffle contract
@@ -37,79 +34,82 @@ import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFCo
  * @dev Implements Chainlink VRF v2.5
  */
 
-
-contract Raffle {
-    /**Error */
+contract Raffle is VRFConsumerBaseV2Plus {
+    /**
+     * Error
+     */
     error Raffle_SendMoreToEnterRaffle();
 
     uint256 private immutable i_entranceFee;
-// dev The duration of the lottery in seconds
-uint256 private immutable i_interval;
-address payable[] private s_players;
-uint256 private s_lastTimeStamp;
+    // dev The duration of the lottery in seconds
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+    uint256 private immutable i_interval;
+    bytes32 private immutable i_keyHash;
+    uint256 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+    address payable[] private s_players;
+    uint256 private s_lastTimeStamp;
 
+    event RaffleEntered(address indexed player);
 
-
-event RaffleEntered(address indexed player);
-
-
-
-
-constructor (uint256 entranceFee, uint256 interval) {
-    i_entranceFee = entranceFee;
-    i_interval = interval;
-    s_lastTimeStamp = block.timestamp;
-}
-
-
-
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        address vrfCoordinator,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2Plus(vrfCoordinator, subscriptionId) {
+        i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
+        i_keyHash = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
+        
+    }
 
     function enterRaffle() external payable {
         // require(msg.value >= i_entranceFee, "Send more to enter raffle");
         // require(msg.value >= i_entranceFee, SendMoreToEnterRaffle());
 
-        if( msg.value < i_entranceFee) {
+        if (msg.value < i_entranceFee) {
             revert Raffle_SendMoreToEnterRaffle();
         }
 
         s_players.push(payable(msg.sender));
 
         emit RaffleEntered(msg.sender);
-
-
-
     }
 
     function pickWinner() external {
         // check to see if enough time has passed
 
         // 1000 - 2000 = 1000
-       if((block.timestamp - s_lastTimeStamp) > i_interval) {
-        revert();
+        if ((block.timestamp - s_lastTimeStamp) > i_interval) {
+            revert();
         }
-        requestId = s_vrfCoordinator.requestRandomWords(
-            VRFV2PlusClient.RandomWordsRequest({
-                keyHash: keyHash,
-                subId: s_subscriptionId,
-                requestConfirmations: requestConfirmations,
-                callbackGasLimit: callbackGasLimit,
-                numWords: numWords,
-                extraArgs: VRFV2PlusClient._argsToBytes(
-                    VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
-                )
-            })
-        )
-        
-        }
-        // Get our
+        VRFV2PlusClient.RandomWordsRequest memory request = VRFV2PlusClient.RandomWordsRequest({
+            keyHash: i_keyHash,
+            subId: i_subscriptionId,
+            requestConfirmations: REQUEST_CONFIRMATIONS,
+            callbackGasLimit: i_callbackGasLimit,
+            numWords: NUM_WORDS,
+            extraArgs: VRFV2PlusClient._argsToBytes(VRFV2PlusClient.ExtraArgsV1({nativePayment: false}))
+        });
+        uint256 requestId = s_vrfCoordinator.requestRandomWords(request);
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] calldata randomWords) internal override {}
+    // Get our
 
     /**
      * Getter Functions
+     * //
      */
-    function getEntranceFee() external view returns (uint256) {
-        return i_entranceFee;
-    }
+    // function getEntranceFee() external view returns (uint256) {
+    //     return i_entranceFee;
+    // }
 }
 
-
-    
